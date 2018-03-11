ddff336af778b0c5e50930b1efbe301dacfbdea9
caphills <32250271+caphills@users.noreply.github.com>
2018-03-11 10:26:32 -0500

    update table name for the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,10 @@
--- create the database and burger table with the required field or column headers
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 DROP TABLE IF EXISTS burgers_db;
-CREATE TABLE burger 
-(
-  id int NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(255) NOT NULL,
+CREATE TABLE burgers (
+  id INT (10) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR (255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  PRIMARY KEY(id)
+  PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,9 @@
 -- inserting seed data for the burgers_db DB
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Chicken Mushroom' true);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Vegie Delight' true);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Philly Cheese Steak', true);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('chicken jalapeno', false);
-INSERT INTO burgers_db (burger_name, devoured) VALUES ('Chicken Spicy', true);
+	INSERT INTO burgers (burger_name, devoured) VALUES (Chicken Mushroom', false);
+	INSERT INTO burgers (burger_name, devoured) VALUES ('Vegie Delight', false);
+	INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheese Steak Burger', false);
+	INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Jalapeno', true);
+
+
 
 
