1a59570e3426b5b578ae517eb4e98db30c6ce295
caphills <32250271+caphills@users.noreply.github.com>
2018-03-11 21:54:23 -0500

    update server.js

--- server.js
+++ server.js
@@ -6,5 +6,24 @@ var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
 
 var app = express ();
+// set up server for static file
+app.use(express.static("public"));
 
+// express app to be able to parse data
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(bodyParser.json());
+
+// handlebars and view engines
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App listening at localhost:" + PORT);
+});
