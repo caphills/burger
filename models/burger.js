3f953e3542648b48b6da17099850c143577d1e8b
caphills <32250271+caphills@users.noreply.github.com>
2018-03-12 19:48:24 -0500

    added comment

--- models/burger.js
+++ models/burger.js
@@ -6,22 +6,21 @@ var burger = {
     orm.selectAll("burgers", function(res) {
       cb(res);
     });
-  }
-
+  },
   // variables cols and vals are arrays
   insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res {
-      cb(res);
-    });
-  }
+    orm.insertOne("burgers", cols, vals, function(res) {
 
-  // The objColVals object specifying columns as obj keys with assoc. values
+    })     
+  },
   updateOne: function(objColVals, condition, cb) {
     orm.updateOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
+     
+    })
   }
-};
+}
+
 
 // export db functions for the burgerController.js
 module.exports = burger;
+
